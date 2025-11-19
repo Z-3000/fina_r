2af16503a49bfafaa4583338189b7fb25d186413
Z-3000 <jhson3907@gmail.com>
2025-11-19 20:27:23 +0900

    fix: Final push of core project files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js 파일 내부
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 이 경로가 가장 중요합니다.
+  ],
+  theme: {
+    // ...
+  },
+  plugins: [],
+}
